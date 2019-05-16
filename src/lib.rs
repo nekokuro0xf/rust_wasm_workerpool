39cbc6b0233317f60d000fc80f794e12137731e8
nekokuro0xf <xiaohei0xf@gmail.com>
2019-05-17 07:42:34 +0900

    upload

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,12 @@
 extern crate stdweb;
 
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen::JsValue;
 use js_sys::Array;
 use web_sys::{ MessageEvent,  Worker };
-use stdweb::private::wasm_bindgen::JsCast;
-use stdweb::private::wasm_bindgen::JsValue;
+// use stdweb::private::wasm_bindgen::JsCast;
+// use stdweb::private::wasm_bindgen::JsValue;
 use std::sync::{Arc, Mutex};
 
 #[wasm_bindgen(start)]
@@ -15,6 +17,8 @@ pub fn main() -> Result<(), JsValue> {
         // alert( @{message} );
         console.log(@{message})
     }
+
+    let worker_pool = WorkPool::create_worker();
     Ok(())
 }
 
@@ -35,7 +39,7 @@ pub struct WorkPool {
 #[wasm_bindgen]
 impl WorkPool{
     #[wasm_bindgen(constructor)]
-    pub fn createWorker() -> Result<WorkPool, JsValue>{
+    pub fn create_worker() -> Result<WorkPool, JsValue>{
         let callback = Closure::wrap(Box::new(|event: MessageEvent| {
             // let Some(msg) = event.dyn_ref::<MessageEvent>();
             let data = event.data();
